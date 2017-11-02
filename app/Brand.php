cb82b17d60833e671063ff2f98e73e025824265a
Semen Khizhnyak <gsem842@gmail.com>
2017-11-02 15:42:30 +0700

    Create relation between Brand, Device and Service

--- app/Brand.php
+++ app/Brand.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Brand extends Model
 {
-    //
+    public function devices()
+    {
+        return $this->hasMany('App\Device');
+    }
 }
--- app/Device.php
+++ app/Device.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Device extends Model
 {
-    //
+    public function brand()
+    {
+        return $this->belongsTo('App\Brand');
+    }
+
+    public function service()
+    {
+        return $this->hasMany('App\Service');
+    }
 }
